8da816484f65fbc6f46ad6a648393a42b4c024aa
akdlx <akdlx@175.210.123.239>
2022-05-18 15:11:26 +0900

    재형 views안에 기부 폴더 생성 및 기부메인페이지.jsp 생성 webapp에 summernote추가, upload폴더(donation) 생성 upload(donation)에 이미지슬라이드 4장추가

--- src/main/java/kr/or/donation/controller/DonationController.java
+++ src/main/java/kr/or/donation/controller/DonationController.java
@@ -2,6 +2,7 @@ package kr.or.donation.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.donation.model.service.DonationService;
 
@@ -9,4 +10,9 @@ import kr.or.donation.model.service.DonationService;
 public class DonationController {
 	@Autowired
 	private DonationService service;
+	
+	@RequestMapping(value="/donationList.kh")
+	public String dmMail() {
+		return "donation/donationMain";
+	}
 }
