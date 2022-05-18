b387302ab5dfd337feef8ab4ddbd01864d198a5d
Moon-write <neverregret_@daum.net>
2022-05-18 15:16:38 +0900

    5.18 (지음) summerNote 관련 (필요한 파일만 두고 삭제)

--- src/main/java/kr/or/auction/controller/AuctionController.java
+++ src/main/java/kr/or/auction/controller/AuctionController.java
@@ -2,6 +2,7 @@ package kr.or.auction.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.auction.model.service.AuctionService;
 
@@ -11,4 +12,14 @@ public class AuctionController {
 	@Autowired
 	private AuctionService service;
 	
+	@RequestMapping(value="auctionList.kh")
+	public String auctionList() {
+		// 진행중인 경매리스트 불러오기 + 페이지순
+		return "auction/auctionList";
+	}
+	
+	@RequestMapping(value="addAuction.kh")
+	public String addAuction() {
+		return "auction/addAuction";
+	}
 }
---
+++ src/main/webapp/WEB-INF/views/auction/auctionList.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>경매</title>
+</head>
+<body>
+	<%@include file="/WEB-INF/views/common/header.jsp" %>
+		<div class="page-content">
+			<div class="page-title">경매</div>
+			<a href="/addAuction.kh">경매 등록</a>
+		</div>
+	<%@include file="/WEB-INF/views/common/footer.jsp"%>
+</body>
+</html>
